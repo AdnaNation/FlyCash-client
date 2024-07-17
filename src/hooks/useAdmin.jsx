13b6b403f6582c55e169f1f32fa10386bf49d13d
AdnaNation <azizulislamadnan@gmail.com>
2024-07-17 15:25:08 +0600

    use admin hook

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+
+const useAdmin = () => {
+  const userEmailOrPhone = localStorage.getItem("userEmailOrPhone");
+  const axiosSecure = useAxiosSecure();
+  const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+    queryKey: [userEmailOrPhone, "isAdmin"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/admin/${userEmailOrPhone}`);
+      console.log(res.data);
+      return res.data?.admin;
+    },
+  });
+  return [isAdmin, isAdminLoading];
+};
+
+export default useAdmin;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import useAdmin from "../../hooks/useAdmin";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+
 const Home = () => {
+  const [isAdmin] = useAdmin();
+  const axiosSecure = useAxiosSecure();
+  const userEmailOrPhone = localStorage.getItem("userEmailOrPhone");
+  const { data: user = {} } = useQuery({
+    queryKey: ["user", userEmailOrPhone],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/user/${userEmailOrPhone}`);
+      return res.data;
+    },
+  });
+  console.log(user);
   return (
     <div>
-      <p className="btn bg-black">this is home</p>
+      <div className="navbar bg-pink-500 text-primary-content rounded">
+        <button className="btn btn-ghost text-xl">FlyCash</button>
+      </div>
+      <p className="">{isAdmin ? "you are admin" : "you are user"}</p>
     </div>
   );
 };
